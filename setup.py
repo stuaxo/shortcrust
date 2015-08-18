bd36ea3c86fbc14a2126287d5300fb039c48a717
Stuart Axon <stuaxo2@yahoo.com>
2015-08-19 02:11:11 +0800

    Update setup.py
    Use setuptools (so that setup.py develop works).    Use find_packages ... install wasn't quite right and couldn't run examples.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
 	name='Shortcrust',
-	version='0.1dev',
-	packages=['shortcrust',],
+	version='0.2dev',
+	packages=find_packages(exclude=['examples']),
 	license='MIT',
 	long_description=open('README.txt').read(),
 )
